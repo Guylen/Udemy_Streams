30ff6c66c9b6ec46e81c79d515d90832cd1332b2
Guylen <guylenoel@gmail.com>
2020-05-25 21:38:38 +1000

    Update authReducer.js

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -10,7 +10,7 @@ export default (state = firstState, action) => {
     case SIGN_IN:
       return { ...state, isSignedIn: true , userId: action.payload};
     case SIGN_OUT:
-      return { ...state, isSIgnedIn: false, userId: null};
+      return { ...state, isSignedIn: false, userId: null};
     default:
       return state;
   }
