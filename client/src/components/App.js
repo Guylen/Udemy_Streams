d485e40b7b0a25e63f6c8fc0cc97f70f3995fe32
Guylen <guylenoel@gmail.com>
2020-07-15 20:52:59 +1000

    Finish

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,8 +18,8 @@ const App = () => {
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/:id" component={StreamShow} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
+          <Route path="/streams/:id" exact component={StreamShow} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
           </Switch>
         </div>
       </Router>
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -2,30 +2,62 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 import { map } from "lodash";
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
   }
-  render() {
-    if(!this.props.stream){
-       return <div>Loading...</div>
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildplayer();
+  }
+
+  componentDidUpdate() {
+    this.buildplayer();
+  }
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildplayer() {
+    if (this.player || !this.props.stream) {
+      return;
     }
 
-    const {title, description} = this.props.stream;
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
 
     return (
-        <div>
-            <h1>{title}</h1>
-            <h5>{description}</h5>
-        </div>
+      <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return {stream: state.streams[ownProps.match.params.id]}
-}
+  return { stream: state.streams[ownProps.match.params.id] };
+};
 
 export default connect(mapStateToProps, { fetchStream })(StreamShow);
 
